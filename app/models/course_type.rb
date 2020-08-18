9fa4c5389bfe2a7cd4bc72c63a04a16768d61847
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-08-18 16:19:35 +0700

    Create header and footer layout

--- app/models/course_type.rb
+++
@@ -1,5 +0,0 @@
-class CourseType < ApplicationRecord
-  has_many :courses
-
-  validates :course_type, presence: true, length: {maximum: Settings.course_type.maximum}
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root 'home#index'
+
+    get "/about", to: "home#about"
     # Ex:- scope :active, -> {where(:active => true)}
     devise_for :users
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
