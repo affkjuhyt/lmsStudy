8370580f50e704745eecb0bab721086952a1bf6a
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-08-19 21:39:38 +0700

    Reset password

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,15 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+var jQuery = require("jquery");
+require("bootstrap");
+
+// import jQuery from "jquery";
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+require('webpack-jquery-ui');
+require('webpack-jquery-ui/css');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
+const erb = require('./loaders/erb')
 
-module.exports = environment
+const webpack = require("webpack");
+
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    jquery: "jquery",
+    "window.Tether": "tether",
+    Popper: ["popper.js", "default"] // for Bootstrap 4
+  })
+);
+
+environment.loaders.prepend('erb', erb)
+module.exports = environment;
