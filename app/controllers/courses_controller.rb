0106b2de429b0fec02f6e01f5aa66a245fa4f779
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-08-20 08:55:46 +0700

    Show details course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,21 @@
 class CoursesController < ApplicationController
+  before_action :get_course
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @courses = Course.all.paginate(page: params[:page], per_page: Settings.search.per_page)
+  end
+
+  def show
+  end
+
+  private
+
+  def get_course
+    @course = Course.find_by(id: params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :overview, :description, :image)
+  end
 end
