06ece73e33e49cb493c14dcbfd5277ae60ad1642
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-08-26 09:57:41 +0700

    Nested routers

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,7 +1,8 @@
 class LessonsController < ApplicationController
-  before_action :load_lesson
+  before_action :load_course
 
   def show
+    @lesson = @course.lessons.find(params[:id])
     @user_course = current_user.user_courses.find_by(course_id: @lesson.course_id)
     respond_to do |format|
       format.js
@@ -10,7 +11,7 @@ class LessonsController < ApplicationController
 
   private
 
-  def load_lesson
-    @lesson = Lesson.find(params[:id])
+  def load_course
+    @course = Course.find(params[:course_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
     root 'home#index'
 
     get "/about", to: "home#about"
-    resources :courses
+    resources :courses do
+      resources :lessons
+    end
     resources :user_courses, only: [:create, :edit, :update]
     resources :review_courses, except: [:new, :show]
     post "course/rate/:course_id", to: "rates#save_rate", as: :rate_course
-    resources :lessons, only: :show 
     # Ex:- scope :active, -> {where(:active => true)}
     devise_for :users, skip: :omniauth_callbacks, controllers: { sessions: "sessions" }
     resources :categories
