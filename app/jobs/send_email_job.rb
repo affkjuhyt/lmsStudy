7f0e0e3fb0fb6c1096bde73224c9d9f366f25c8c
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-08-31 09:03:14 +0700

    Send mail background job

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -2,6 +2,10 @@ class SendEmailJob < ApplicationJob
   queue_as :default
 
   def perform course
+<<<<<<< HEAD
     CourseMailer.course_email(course).deliver_now
+=======
+     CourseMailer.welcome_email(course).deliver_now
+>>>>>>> Send mail background job
   end
 end
