8a346f1cf306d3c0c1680c9ea490cb704e043a37
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-09-01 17:26:45 +0700

    Create layout admin lte

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,4 +1,9 @@
 module CoursesHelper
+  def is_admin?
+    return unless current_user
+    current_user.is_admin?
+  end
+
   def course_registered_by_user?(course_id)
     return unless current_user
     current_user.user_course_ids.include?(course_id)
