8e8774a2ffd825dbd71e54008b1b258e5b373f4e
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-09-02 09:01:52 +0700

    Create CRUD user

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,56 @@
+class Admin::UsersController < Admin::BaseController
+  before_action :load_user, only: [:show, :edit, :update]
+
+  def index
+    @result = User.ransack params[:q]
+    @users = @result.result(distinct: true).order(created_at: :desc).not_admin.
+      paginate(page: params[:page], per_page: Settings.users.per_page)
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def edit; end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to admin_users_path, notice: 'Created user' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to admin_users_path, notice: 'User updated' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def load_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :avatar, :email)
+  end
+end
