ec964e2e2beaf0d16ffe4593bbb2da050cb00223
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-09-04 09:50:44 +0700

    Remove validate max point

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,15 +5,7 @@ class Lesson < ApplicationRecord
   has_many :exams, dependent: :destroy
   validates :lesson_type, :sequence, :name, presence: true
   validates :name, length: { maximum: 14 }
-  validates :check_point, numericality: { only_integer: true, greater_than_or_equal_to: 2 }, presence: true
+  validates :check_point, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, presence: true
   validates :video_url, presence: true
   enum lesson_type: Settings.lesson_type.to_h
-
-  validate :max_point
-
-  def max_point
-    if check_point && check_point > questions.length
-      errors.add(:check_point, "can't greater than number of questions")
-    end
-  end
 end
