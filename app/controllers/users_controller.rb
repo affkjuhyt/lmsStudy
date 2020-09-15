b79b92b14e1192e2cf12b1def42ebc9e8c552ce0
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-09-15 10:40:09 +0700

    Fix signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
-  before_action :get_user
+  before_action :load_user
+  before_action :authenticate_user!
+  before_action :check_authorization
 
   private
 
-  def get_user
+  def load_user
     @user = User.find(params[:id])
+  end
+
+  def check_authorization
+    unless authorization
+      flash[:notice] = t('devise.permission')
+      redirect_to root_url
+    end
+  end
+
+  def authorization
+    (current_user.id == @user.id) || current_user.is_admin?
   end
 
   def user_params
