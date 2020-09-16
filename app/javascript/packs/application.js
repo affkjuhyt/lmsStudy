b176baa839977a3619dce921adad74bff73bc2fb
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-09-16 20:57:46 +0700

    Back to top

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,9 +23,11 @@ require('webpack-jquery-ui/css');
 
 require("packs/pagination");
 require("packs/courseLike");
-require("packs/disable.js");
+require("packs/disable");
 require("packs/admin_course");
+require("packs/collapse");
 require("packs/scroll");
+require("packs/backToTop");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
---
+++ app/javascript/packs/collapse.js
@@ -0,0 +1,38 @@
+$(document).on("input", ".input-lesson-name", function () {
+  $(this).closest(".card").find(".lesson-name-collapse").text($(this).val());
+})
+
+$(window).on("turbolinks:load", function () {
+  $(".edit-collapse").each(function () {
+    $(this).closest(".card").find(".card-body").first().slideUp();
+  });
+  $(".prev-lesson").each(function () {
+    $(this).text($(this).closest(".card").find(".input-lesson-name").val());
+  });
+  $(".collapse-show-lesson-admin").slideUp();
+})
+
+$(document).on("click", ".edit-collapse, .btn-collapse, .lesson-name-collapse", function () {
+  quiz = $(this).closest(".card").find(".quiz-form");
+  video = $(this).closest(".card").find(".card-body").first();
+  lesson_type = $(this).closest(".card").find(".video").is(":checked");
+  $(this).closest(".header-lesson").find(".btn-collapse").toggleClass("fa-angle-double-down fa-angle-double-up");
+  if (!lesson_type && $(quiz).is(":visible")) {
+    quiz.slideUp();
+    video.slideUp();
+  } else if(!lesson_type && $(quiz).is(":hidden")){
+    quiz.slideDown();
+    video.slideDown();
+  } else if(lesson_type && $(video).is(":visible")){
+    video.slideUp();
+    quiz.hide();
+  } else if(lesson_type && $(video).is(":hidden")){
+    video.slideDown();
+    quiz.hide();
+  }
+})
+
+$(document).on("click", ".btn-collapse-show-admin", function () {
+  $(this).closest(".card").find(".collapse-show-lesson-admin").slideToggle();
+  $(this).toggleClass("fa-angle-double-down fa-angle-double-up");
+})
