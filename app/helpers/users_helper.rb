d34d10bd513b26c93eb4b2cc308bdd24339ef763
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-09-25 17:37:12 +0700

    Fix bug

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,13 +3,13 @@ module UsersHelper
     cnt = 0
     users.each do |user|
       lesson_step = user.user_courses.find_by(course_id: course.id).lesson_step
-      if lesson_step == lessons.size
+      if lesson_step == @lessons.size
         cnt += 1
       end
     end
 
     register = course.total_register_count
-    
+
     unless register.nil?
       percent = (cnt.to_f / register) * 100
       percent.round
